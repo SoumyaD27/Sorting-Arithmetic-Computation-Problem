e98ba1e3951ce66be8c0c22577604ba081e020a7
SoumyaD27 <soumyad233@gmail.com>
2022-11-19 13:28:55 +0530

    [Soumya]-Store the result in dictionary

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -4,6 +4,14 @@ read -p "Enter a first number: " a
 read -p "Enter a second number: " b
 read -p "Enter a thrid number: " c
 
+declare -A test_dict
 
-d=$(awk 'BEGIN {print '$a' % '$b' + '$c'}')
-echo $d
+i=$(( $a + $b * $c ))
+j=$(( $a * $b + $c ))
+k=$(awk 'BEGIN {print '$c' + '$a' / '$c'}')
+l=$(awk 'BEGIN {print '$a' % '$b' + '$c'}')
+test_dict[key1]=$i
+test_dict[key2]=$j
+test_dict[key3]=$k
+test_dict[key4]=$l
+
