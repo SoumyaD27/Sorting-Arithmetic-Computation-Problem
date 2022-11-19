25f97c5103bacd4455bcb6bb9b56011a474bc2db
SoumyaD27 <soumyad233@gmail.com>
2022-11-19 13:03:32 +0530

    [Soumya]-Computing the three inputs

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 read -p "Enter a first number: " a
 read -p "Enter a second number: " b
 read -p "Enter a thrid number: " c
 
-sum=$(( $a + $b * $c ))
-echo $sum
+isPartTime=8
+
+d=$(( $a * $b + $c ))
+echo $d
