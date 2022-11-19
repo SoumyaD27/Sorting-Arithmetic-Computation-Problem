57c4dc88cfb688dd7870ce28551fd90d24b33d88
SoumyaD27 <soumyad233@gmail.com>
2022-11-19 13:35:56 +0530

    [Soumya]-Read from dictionary

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -10,8 +10,13 @@ i=$(( $a + $b * $c ))
 j=$(( $a * $b + $c ))
 k=$(awk 'BEGIN {print '$c' + '$a' / '$c'}')
 l=$(awk 'BEGIN {print '$a' % '$b' + '$c'}')
+
 test_dict[key1]=$i
 test_dict[key2]=$j
 test_dict[key3]=$k
 test_dict[key4]=$l
 
+echo ${test_dict[key1]}
+echo ${test_dict[key2]}
+echo ${test_dict[key3]}
+echo ${test_dict[key4]}
